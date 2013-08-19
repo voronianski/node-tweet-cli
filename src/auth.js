0017ac3a06326d1c4b60420b11594611296b8075
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2013-08-19 22:55:37 +0300

    auth module

---
+++ src/auth.js
@@ -0,0 +1,29 @@
+var OAuth = require('oauth').OAuth;
+
+var oauth = new OAuth(
+	'http://api.twitter.com/oauth/request_token',
+	'http://api.twitter.com/oauth/access_token',
+	'aTiLjvl8MuW9MG12DXng',
+	'b2ceLIWbvrO6Xj8VkZ6NxPIwu3e4dHSSvHa0QjGA',
+	'1.0',
+	null,
+	'HMAC-SHA1'
+);
+
+exports.getRequestToken = function (callback) {
+	oauth.getOAuthRequestToken(function (err, requestToken, requestTokenSecret) {
+		if (err) {
+			return callback(err);
+		}
+		callback(null, requestToken, requestTokenSecret);
+	});
+};
+
+exports.getAccessToken = function (requestToken, requestTokenSecret, pin, callback) {
+	oauth.getOAuthAccessToken(function (err, accessToken, accessTokenSecret) {
+		if (err) {
+			return callback(err);
+		}
+		callback(null, accessToken, accessTokenSecret);
+	});
+};
