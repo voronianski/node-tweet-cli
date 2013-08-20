f51f952fb7a0a8586bf28b10523c571ae73ac31d
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2013-08-20 23:16:07 +0300

    add missed files

---
+++ src/errors.js
@@ -0,0 +1,12 @@
+var cli = require('./cli');
+
+module.exports = function (error, message) {
+	error = JSON.stringify(error, '', '\t');
+	message = message || 'Error while executing command!';
+
+	cli.log.error(message);
+
+	if (cli.argv.debug) {
+		throw new Error(error);
+	}
+};
