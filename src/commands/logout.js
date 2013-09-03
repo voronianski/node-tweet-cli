eade6f4275de5030bc1cb6ff14527672a64e9311
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2013-09-03 21:58:38 +0200

    logout cmd

---
+++ src/commands/logout.js
@@ -0,0 +1,30 @@
+var cli = require('../cli');
+var db = require('../common/db');
+var errorHandler = require('../common/errors');
+
+var logout = function () {
+	db.removeActiveUser(function (err, num) {
+		if (err) {
+			return errorHandler(err, 'Error occured while unauthorizing user');
+		}
+
+		if (!num) {
+			cli.log.warn('There is no twitter to unauthorize!');
+			cli.log.info('Use ' + '`tweet login`'.magenta + ' command to be able to create tweets.');
+			return;
+		}
+
+		cli.log.info('Twitter account was unauthorized!');
+	});
+};
+
+logout.usage = [
+	'The ' + '`tweet logout`'.magenta + ' command unauthorize',
+	'your @twitter account from node-tweet-cli',
+	'',
+	'Usage:'.cyan.bold.underline,
+	'',
+	' tweet logout'
+];
+
+module.exports = logout;
