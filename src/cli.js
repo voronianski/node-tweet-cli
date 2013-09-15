5136adf897efd45be53dcc41e63138ffbf600c17
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2013-09-16 00:41:01 +0300

    remove console.log

--- src/cli.js
+++ src/cli.js
@@ -14,8 +14,6 @@ require('pkginfo')(module, 'name', 'version');
 
 app.config.file({ file: path.join(__dirname, '..', 'config.json') });
 
-console.log(app.config);
-
 app.use(flatiron.plugins.cli, {
 	version: true,
 	usage: require('./common/usage.js'),
