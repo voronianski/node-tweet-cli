e0919b14aa86b654a1022b86e5071936b25a3b0a
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2013-10-18 16:12:01 +0300

    tweet read command improvements

--- src/commands/read.js
+++ src/commands/read.js
@@ -1,63 +1,49 @@
+var readline = require('readline');
 var cli = require('../cli');
 var api = require('../common/api');
 var db = require('../common/db');
 var errorHandler = require('../common/errors');
-var readline = require('readline');
 
-var readTweet = function () {
-  db.getActiveUser(function (err, user) {
-    if (err) {
-      return errorHandler(err);
-    }
+var read = function () {
+	db.getActiveUser(function (err, user) {
+		if (err) {
+			return errorHandler(err);
+		}
 
-    if (!user) {
-      cli.log.warn('Please login with ' +
-        '`tweet login`'.magenta +
-          ' command first, before trying to tweet.');
-      process.exit();
-    }
-    /**
-     * Asynchronously posts to Twitter.
-     *
-     * @param {string} tweet
-     *     Message to be posted. Note: should be no greater than 140
-     *     characters.
-     */
-    var postTweet = function (tweet) {
-      api.post(tweet, user, function (err, response, body) {
-        if (err) {
-          return errorHandler(err);
-        }
-        if (body.errors) {
-          err = body.errors[0];
-          var msg = err.code === 186 ?
-                    'Oops! Your tweet is over 140 characters.' :
-                    null;
-          return errorHandler(err, msg);
-        }
-      });
-    }
+		if (!user) {
+			cli.log.warn('Please login with ' + '`tweet login`'.magenta + ' command to be able to create tweets.');
+			process.exit();
+		}
 
-    var rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout
-    });
-    rl.on('line', function(tweet) {
-      postTweet(tweet.slice(0, 140));
-      rl.close();
-    });
-  });
+		var rl = readline.createInterface({
+			input: process.stdin,
+			output: process.stdout
+		});
+
+		rl.on('line', function (tweet) {
+			postTweet(tweet.slice(0, 140));
+			rl.close();
+		});
+
+		function postTweet (tweet) {
+			api.post(tweet, user, function (err, response, body) {
+				if (err) {
+					return errorHandler(err);
+				}
+				cli.log.info('Success! Your tweet was published.');
+			});
+		}
+	});
 };
 
-readTweet.usage = [
-  'The ' + '`tweet read`'.magenta + ' command is identical to ',
-  '`create`'.magenta + ', but reads from stdin, enabling use with scripting.',
-  'eg: ' + '`your_script | tweet read`'.magenta
-
-  'Usage:'.cyan.bold.underline,
-  '',
-  ' echo "usage test" | tweet read'
-  ' tweet read < someInput'
+read.usage = [
+	'The ' + '`tweet read`'.magenta + ' command posts to twitter',
+	'but reads from stdin, enabling use with scripting.',
+	'',
+	'Usage:'.cyan.bold.underline,
+	'',
+	' your_script | tweet read',
+	' tweet read < someInput'
 ];
 
-module.exports = readTweet;
+module.exports = read;
