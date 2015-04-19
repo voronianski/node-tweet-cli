c3411ff4a421ca62e98a3265bc281743c9e5d61f
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2015-04-19 16:28:02 +0300

    small update

--- src/commands/stream.js
+++ src/commands/stream.js
@@ -2,33 +2,32 @@ var cli = require('../cli');
 var api = require('../common/api');
 var db = require('../common/db');
 var errorHandler = require('../common/errors');
-var colors = require('colors');
 
 var stream = function (query) {
-    db.getActiveUser(function (err, user) {
-        if (err) {
-            return errorHandler(err);
-        }
+	db.getActiveUser(function (err, user) {
+		if (err) {
+			return errorHandler(err);
+		}
 
-        if (!user) {
-            cli.log.warn('Please login with ' + '`tweet login`'.magenta + ' command to be able to create tweets.');
-            process.exit();
-        }
+		if (!user) {
+			cli.log.warn('Please login with ' + '`tweet login`'.magenta + ' command to be able to create tweets.');
+			process.exit();
+		}
 
 
-        api.stream(query, user, function(tweet) {
-            if (tweet && tweet.user) {
-                if (cli.argv.json) {
-                    console.log(JSON.stringify(tweet));
-                } else {
-                    console.log(
-                        ('@'+tweet.user.screen_name+':').cyan.bold,
-                        tweet.text.replace(/[\n\r]+/g,'')
-                    );
-                }
-            }
-        });
-    });
+		api.stream(query, user, function(tweet) {
+			if (tweet && tweet.user) {
+				if (cli.argv.json) {
+					console.log(JSON.stringify(tweet));
+				} else {
+					console.log(
+						('@'+tweet.user.screen_name+':').cyan.bold,
+						tweet.text.replace(/[\n\r]+/g,'')
+					);
+				}
+			}
+		});
+	});
 };
 
 module.exports = stream;
