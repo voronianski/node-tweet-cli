2bbe5817146e7ca3ce726f38904b5987ebb52f8c
Victor Perin <me@victorperin.com>
2015-05-07 11:23:49 -0300

    Adding timeline function

--- src/common/api.js
+++ src/common/api.js
@@ -4,6 +4,7 @@ var es = require('event-stream');
 
 var API = 'https://api.twitter.com/1.1';
 var STREAMAPI = 'https://stream.twitter.com/1.1';
+var USERSTREAMAPI = 'https://userstream.twitter.com/1.1';
 
 exports.post = function (tweet, user, options, callback) {
 	if (!tweet || typeof tweet !== 'string') {
@@ -47,6 +48,44 @@ exports.stream = function(query, user, options, callback) {
 	}
 
 	var uri = STREAMAPI + '/statuses/filter.json?track=' + fullURIEncode(query);
+	var headers = { 'Content-Type': 'application/json', 'User-Agent': 'node-tweet-cli' };
+
+	var oauth = {
+		consumer_key: cli.config.get('consumerKey'),
+		consumer_secret: cli.config.get('consumerSecret'),
+		token: user.accessToken,
+		token_secret: user.accessTokenSecret
+	};
+
+	request.post({ uri: uri, headers: headers, oauth: oauth, json: true })
+	.pipe(es.map(function(buffer,cb) {
+		cb(null, buffer.toString('utf8'))
+	}))
+	.pipe(es.split(function(str) {
+		//silently skip parsing errors to keep the stream running
+		try {
+			return JSON.parse(str);
+		} catch(e) {
+			process.stderr.write('error parsing:'+str+'\n');
+			return;
+		}
+	}))
+	.on('data',function(data) {
+		callback(data);
+	});
+};
+
+exports.timeline = function(user, options, callback) {
+	if (!user) {
+		return callback('Twitter account is not specified.');
+	}
+
+	if (typeof options === 'function') {
+		callback = options;
+		options = {};
+	}
+
+	var uri = USERSTREAMAPI + '/user.json?with=followings';
 	var headers = { 'Content-Type': 'application/json', 'User-Agent': 'node-tweet-cli' };
 
 	var oauth = {
